ac8483766586759dc3f6e35b4a850759f7dce655
disc0dancer <dj@hfour.com>
2020-03-15 01:37:16 +0100

    Engine supports subdirectories now

--- engine/index.ts
+++ engine/index.ts
@@ -1,7 +1,7 @@
-import { readdirSync, readFileSync, fstat, writeFileSync, stat, statSync, unlinkSync } from 'fs';
-import { normalize } from 'path';
 import matter from 'gray-matter';
 import marked from 'marked';
+import { normalize } from 'path';
+import { readdirSync, readFileSync, writeFileSync, statSync, unlinkSync, rmdirSync, mkdirSync } from 'fs';
 
 let config = {
   index: "index.md",
@@ -10,87 +10,96 @@ let config = {
   nav: [] as { title: string; link?: string }[]
 }
 
+const reservedDirs = ['assets', 'engine', 'extras', 'uploads', 'source'];
 const rootPath = normalize(`${__dirname}/..`);
 const sourceDirPath = normalize(`${rootPath}/source`);
 const assetsDirPath = normalize(`${rootPath}/assets`);
 
-let sources: string[] = [];
+// Read the template upfront - we only have a single template for now
+let TEMPLATE: string = "";
 try {
-  sources = readdirSync(sourceDirPath);
-} catch {
-  console.log(`Invalid source directory ${sourceDirPath}`);
-  process.exit(1);
-}
-
-if (!sources.find((fname) => fname === "_config.json")) {
-  console.log(`Source directory ${sourceDirPath} does not contain a _config.json file`);
-  process.exit(1);
-}
-
-sources = sources.filter((s) => s !== "_config.json");
-
-try {
-  const configStr = readFileSync(normalize(`${sourceDirPath}/_config.json`), 'utf-8');
-  config = Object.assign(config, JSON.parse(configStr));
-} catch {
-  console.log("Invalid source/_config.json file.");
-  process.exit(1);
-}
-
-let assets: string[] = [];
-try {
-  assets = readdirSync(assetsDirPath);
-} catch {
-  console.log(`Invalid assets directory ${assetsDirPath}`);
-  process.exit(1);
-}
-
-let template: string = "";
-try {
-  template = readFileSync(normalize(`${assetsDirPath}/template.html`), 'utf-8');
+  TEMPLATE = readFileSync(normalize(`${assetsDirPath}/template.html`), 'utf-8');
 } catch {
   console.log("Invalid assets/template.html file.");
   process.exit(1);
 }
 
-const navHTML = config.nav.map((obj) => {
-  if (!obj.link) {
-    return `<span>${obj.title}</span>`;
-  } else if (obj.link.match(/^(http(s)?:\/\/)/)) {
-    return `<a href="${obj.link}">${obj.title}</a>`;
-  } else {
-    if (sources.findIndex((s) => s === obj.link) === -1) {
-      console.warn(
-        `Broken link: "${obj.title}" links to "${obj.link}" which is not in the source dir`);
-    }
-    const ext = obj.link.substring(obj.link.lastIndexOf('.'));
-    const fname = obj.link.substring(0, obj.link.length - ext.length);
-    return `<a href="/${fname}.html">${obj.title}</a>`;
-  }
-}).join('\n');
-
-
-// Clean up old html pages before generating new ones
+// Recursively clean up old html pages before generating new ones
 // Important so that we remove htmls when we remove source .md files.
-readdirSync(rootPath).filter(fname => fname.endsWith('.html')).forEach(fname => {
-  const pth = normalize(`${rootPath}/${fname}`);
-  try {
-    const stats = statSync(pth);
-    if (stats.isFile()) {
-      unlinkSync(pth);
+function cleanUp(dirPath: string = rootPath) {
+  readdirSync(dirPath).forEach(fname => {
+    const filePath = `${dirPath}/${fname}`;
+    const stats = statSync(normalize(filePath));
+    if (stats.isDirectory() && reservedDirs.find(name => name === fname)) {
+      cleanUp(filePath);
+    } else if (stats.isFile() && (fname.endsWith('html') || fname === '_config.json')) {
+      unlinkSync(filePath);
     }
-  } catch {
-    console.log(`Can't stat or unlink ${pth}`);
+  });
+  if (dirPath !== rootPath) {
+    rmdirSync(dirPath);
   }
-});
+}
 
-sources.forEach(createPage);
+// Recursively parse the source dir and create htmls in the root dir
+// at the equivalent location
+function parseSources(dirPath: string = sourceDirPath) {
+  let sources: string[] = [];
 
-/**
- * Creates a page from a source.
- * @param source String the source filename.
- */
-function createPage(fname: string) {
+  if (dirPath !== sourceDirPath) {
+    mkdirSync(dirPath);
+  }
+
+  try {
+    sources = readdirSync(sourceDirPath);
+  } catch {
+    console.log(`Invalid source directory ${sourceDirPath}`);
+    process.exit(1);
+  }
+
+  if (!sources.find((fname) => fname === "_config.json")) {
+    console.log(`Source directory ${sourceDirPath} does not contain a _config.json file`);
+    process.exit(1);
+  }
+
+  sources = sources.filter((s) => s !== "_config.json");
+
+  try {
+    const configStr = readFileSync(normalize(`${sourceDirPath}/_config.json`), 'utf-8');
+    config = Object.assign({}, config, JSON.parse(configStr));
+  } catch {
+    console.log("Invalid source/_config.json file.");
+    process.exit(1);
+  }
+
+  const navHTML = config.nav.map((obj) => {
+    if (!obj.link) {
+      return `<span>${obj.title}</span>`;
+    } else if (obj.link.match(/^(http(s)?:\/\/)/)) {
+      return `<a href="${obj.link}">${obj.title}</a>`;
+    } else {
+      if (sources.findIndex((s) => s === obj.link) === -1) {
+        console.warn(
+          `Broken link: "${obj.title}" links to "${obj.link}" which is not in the source dir`);
+      }
+      const ext = obj.link.substring(obj.link.lastIndexOf('.'));
+      const fname = obj.link.substring(0, obj.link.length - ext.length);
+      return `<a href="/${fname}.html">${obj.title}</a>`;
+    }
+  }).join('\n');
+
+  sources.forEach((name) => {
+    if (statSync(normalize(`${sourceDirPath}/${name}`)).isDirectory()) {
+      parseSources(`${dirPath}/${name}`);
+    } else {
+      createPage(name, navHTML, `${rootPath}/${dirPath.substr(sourceDirPath.length)}`);
+    }
+  });
+}
+
+// Creates a html page from a source markdown file.
+// Places it in a the same directory relative to the site root.
+function createPage(fname: string, navHTML: string, dirPath: string = rootPath) {
   const isMarkdown = fname.endsWith(".md");
   const isHTML = fname.endsWith(".html");
   const { content, data } = matter.read(normalize(`${sourceDirPath}/${fname}`));
@@ -98,7 +107,7 @@ function createPage(fname: string) {
   if (isMarkdown) {
     const pagename = fname.replace(/\.md$/, "");
     const html = marked(content);
-    const result = template.replace(/\{\{(.+)\}\}/g, (_match, varName) => {
+    const result = TEMPLATE.replace(/\{\{(.+)\}\}/g, (_match, varName) => {
       switch(varName) {
         case "meta-title":
           return data.title ? `${config.titlePrefix} - ${data.title}` : config.titlePrefix;
@@ -119,6 +128,9 @@ function createPage(fname: string) {
           return `{{ -?- ${varName} -?- }}`;
       }
     });
-    writeFileSync(normalize(`${rootPath}/${pagename}.html`), result, 'utf-8');
+    writeFileSync(normalize(`${dirPath}/${pagename}.html`), result, 'utf-8');
   }
 }
+
+cleanUp();
+parseSources();
---
+++ source/quarantine/day-one.md
@@ -0,0 +1,24 @@
+---
+title: Day One in Quarantine
+date: 2020-03-14
+---
+
+This blog series is dedicated to my good friend Sara Sollors. I will write about my 2 week self-imposed isolation due to the COVID-19 pandemic.
+
+I just landed from London and my government has issued an order that I need to stay at home for 14 days because the UK is, according to our ministry of health, a "medium risk" country. Really, this just means that the UK is slightly more infested than Macedonia, but I think that there was a tiny bit of satisfaction to get back at them for having a super expensive visa regime towards us.
+
+When I traveled to London 4 days ago the situation was ... meh. Optimistic as I am, I did not expect that Macedonia, of all places, would pose travel restrictions to the UK, but there you go.
+
+As soon as we landed back in Skopje, UK got hit by our governmental banhammer and passengers arriving from there will be sent back. This clip is a good depiction of how I felt:
+
+https://www.youtube.com/watch?v=_LRpIXhOCmM
+
+Then I learned that our government is not *that* crazy after all and the ban is only valid for foreign passengers, but still - had we arrived a day later we might have been put in a ... non-self-imposed isolation which I don't think has WiFi.
+
+Anyway - feels good to be back, would have been terrible to be stuck in the UK or in a Macedonian quarantine. Not the least because, if not corona, then my wife would have definitely killed me so she can dance the "I told you so" dance on my grave. She did tell me not to go.
+
+Also - No symptoms. I am healthy. Stay tuned.
+
+
+
+
